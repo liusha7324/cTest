9f55aa6856f71ab697e01a4cb10138e5b21be730
liusha7324 <liusha7324@163.com>
2016-08-10 10:59:57 +0800

    changes log 2016-7-5

--- RN8302B CODE/Release_Code/MB4_64A/PowerUpRetort.h
+++
@@ -1,8 +0,0 @@
-/*----------PowerUpRetor.h-----------------*/
-#ifndef	_PowerUpRetor_h
-#define	_PowerUpRetor_h
-
-extern void PowerUpRetort(void);
-extern void PowerUpYearBill(void);
-extern void Check_Energy_main(unsigned char kind);
-#endif
